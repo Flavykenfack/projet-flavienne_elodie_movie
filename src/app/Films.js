91af9b90854bb7679b20f0155e985faf53a4368a
elodieandri <andrielodie@gmail.com>
2016-06-30 10:54:57 +0200

    Ajout des images dans la vue Panier et film
    Modification de la mise en page et rectification de la fonction pour
    ajouter et supprimer un film

--- src/app/Films.js
+++ src/app/Films.js
@@ -2,23 +2,28 @@ angular
   .module('app')
   .component('films', {
     templateUrl: 'app/films.html',
-    controller: function (Enregistrer, Supprimer, $http) {
+    controller: function (Enregistrer, $http) {
       var $ctrl = this;
       $http.get('http://amc.ig.he-arc.ch:3003/movie/upcoming?language=fr')
         .then(function (result) {
           $ctrl.films = result.data.results;
         });
 
-      $ctrl.listeFilms = [];
+      $ctrl.listeFilms = Enregistrer.charger('PanierSerializable');
 
       $ctrl.addItem = function (el) {
         $ctrl.listeFilms.push(el);
         Enregistrer.enregistrer($ctrl.listeFilms);
       };
-
-      $ctrl.delItem = function (item) {
-        Supprimer.supprimer(item);
-        Enregistrer.enregistrer($ctrl.listeFilms);
+      $ctrl.movieDansMaListe = function (testMovieId) {
+        for (var i = 0, l = $ctrl.myMovies.length; i < l; ++i) {
+          if ($ctrl.myMovies[i].id === testMovieId) {
+            return true;
+          }
+        }
+        return false;
       };
+
+
     }
   });
